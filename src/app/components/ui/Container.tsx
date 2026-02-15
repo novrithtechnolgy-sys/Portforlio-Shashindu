fd3b7c5d98c18fdb08ae34bfe5b116bedbfc3bc1
GSNSAMPATH <novrithtechnolgy@gmail.com>
2026-02-15 11:57:44 +0530

    update mobile size

--- src/app/components/ui/Container.tsx
+++ src/app/components/ui/Container.tsx
@@ -1,4 +1,4 @@
 export default function Container({ children }: { children: React.ReactNode }) {
-  return <div className="mx-auto w-full bg-black"><div className="mx-auto w-full max-w-7xl px-8">
+  return <div className="mx-auto w-full bg-black"><div className="mx-auto w-full max-w-7xl px-4 sm:px-8">
     {children}</div></div>;
 }
