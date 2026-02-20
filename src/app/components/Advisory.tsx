bb688764063757854ec1428b71b6137058e7504e
GSNSAMPATH <novrithtechnolgy@gmail.com>
2026-02-20 10:31:37 +0530

    update advisory section

--- src/app/components/Advisory.tsx
+++ src/app/components/Advisory.tsx
@@ -36,7 +36,7 @@ export default function Advisory() {
             </h3>
 
             <p className="mt-6 font-[helvetica] md:text-[20px] max-w-xl text-white leading-8 mx-auto md:mx-0">
-              The General Manager Taking over operations at age 19,
+              Taking over operations at age 19,
               I have led the strategic growth and guest excellence of this luxury property. My role
               involves overseeing daily operations, maintaining high service standards and ensuring
               financial sustainability in a competitive tourism market.
